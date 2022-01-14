6ee6ea6e63813ad189893b31b04b21b0255cc01b
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2022-01-14 08:55:12 -0500

    Initial commit

---
+++ Assets/Script/HealthCollectible.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void start()
+    {
+
+    }
+
+
+    void update()
+    {
+
+    }
+
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            if(controller.currentHealth < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
+
